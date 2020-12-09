bca1f9d0a55f95b5c6940879879cfdc8b9c4fa38
bqla <979052538@qq.com>
2020-12-10 02:07:12 +0800

    bql~~

---
+++ h5/vue.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const utils = {
+    assetsPath: function (_path) {
+        const assetsSubDirectory = process.env.NODE_ENV === 'production'
+            // 生产环境下的 static 路径
+            ? 'static'
+            // 开发环境下的 static 路径
+            : 'static'
+
+        return path.posix.join(assetsSubDirectory, _path)
+    },
+    resolve: function(dir) {
+        return path.join(__dirname, '..', dir)
+    }
+}
+module.exports={
+    publicPath : './',
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://acquacap.com/acquacap', //对应自己的接口
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    },
+    configureWebpack:{
+        module:{
+            rules:[
+                {
+                    test:/\.(woff2?|eot|ttf|otf)(\?.*)$/,
+                    loader:'url-loader',
+                    options:{
+                        limit: 10000,
+                        name: utils.assetsPath('fonrs/[name].[hash:7].[ext]')
+                    }
+                }
+            ]
+        },
+    }
+}
